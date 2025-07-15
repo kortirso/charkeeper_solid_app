a592f75d4d556e00298f43be48865cdf5971600c
Bogdanov Anton <kortirso@gmail.com>
2025-07-15 12:02:23 +0300

    add endpoint for fetching all homebrews

--- pages/Content/Homebrew/Daggerheart/NewRaceForm.jsx
+++ pages/Content/Homebrew/Daggerheart/NewRaceForm.jsx
@@ -1,22 +1,22 @@
 import { createSignal } from 'solid-js';
 import * as i18n from '@solid-primitives/i18n';
 
-import { Input, Select, Button } from '../../../../components';
+import { Input, Button } from '../../../../components';
 import config from '../../../../data/daggerheart.json';
 import { useAppLocale } from '../../../../context';
 
 export const NewDaggerheartRaceForm = (props) => {
   const [name, setName] = createSignal('');
-  const [domains, setDomains] = createSignal([]);
+  // const [domains, setDomains] = createSignal([]);
 
   const [locale, dict] = useAppLocale();
 
   const t = i18n.translator(dict);
 
-  const updateDomains = (value) => {
-    if (domains().includes(value)) setDomains(domains().filter((item) => item !== value));
-    else setDomains(domains().concat(value));
-  }
+  // const updateDomains = (value) => {
+  //   if (domains().includes(value)) setDomains(domains().filter((item) => item !== value));
+  //   else setDomains(domains().concat(value));
+  // }
 
   return (
     <>
@@ -26,17 +26,17 @@ export const NewDaggerheartRaceForm = (props) => {
         value={name()}
         onInput={(value) => setName(value)}
       />
-      <Select
+      {/*<Select
         multi
         containerClassList="mb-4"
         labelText={t('pages.homebrewPage.daggerheart.domains')}
         items={Object.fromEntries(Object.entries(config.domains).map(([key, values]) => [key, values.name[locale()]]))}
         selectedValues={domains()}
         onSelect={updateDomains}
-      />
+      />*/}
       <div class="flex gap-4 w-full">
         <Button outlined classList="flex-1" onClick={props.onCancel}>{t('cancel')}</Button>
-        <Button default classList="flex-1" onClick={() => props.onSave({ brewery: { name: name(), domains: domains() } })}>{t('save')}</Button>
+        <Button default classList="flex-1" onClick={() => props.onSave({ brewery: { name: name() } })}>{t('save')}</Button>
       </div>
     </>
   );
--- pages/Content/Homebrew/Daggerheart/Race.jsx
+++ pages/Content/Homebrew/Daggerheart/Race.jsx
@@ -2,7 +2,7 @@
 import * as i18n from '@solid-primitives/i18n';
 
 // import { Input, Select, Button } from '../../../../components';
-import config from '../../../../data/daggerheart.json';
+// import config from '../../../../data/daggerheart.json';
 import { useAppLocale } from '../../../../context';
 
 export const DaggerheartRace = (props) => {
@@ -11,8 +11,6 @@ export const DaggerheartRace = (props) => {
   const t = i18n.translator(dict);
 
   return (
-    <>
-      <p>{t('pages.homebrewPage.daggerheart.domains')} - {Object.entries(config.domains).filter(([key,]) => props.race.domains.includes(key)).map(([, values]) => values.name[locale()]).join(', ')}</p>
-    </>
+    <></>
   );
 }
