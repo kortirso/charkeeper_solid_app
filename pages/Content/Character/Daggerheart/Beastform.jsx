037a0f5c6d6502a7c280cf4be9ecb216c346c78f
Bogdanov Anton <kortirso@gmail.com>
2025-10-30 19:10:33 +0300

    IS-367 modified rendering Daggerheart beastforms

--- pages/Content/Character/Daggerheart/Beastform.jsx
+++ pages/Content/Character/Daggerheart/Beastform.jsx
@@ -6,6 +6,19 @@ import config from '../../../../data/daggerheart.json';
 import { useAppState, useAppLocale, useAppAlert } from '../../../../context';
 import { updateCharacterRequest } from '../../../../requests/updateCharacterRequest';
 
+const translation = {
+  en: {
+    desc: 'While in Beastform, you cannot use weapons or cast spells from Domain Cards. You can still use class features, abilities, and Beastform-specific actions.',
+    examples: 'Examples: ',
+    adv: 'Advantages on:'
+  },
+  ru: {
+    desc: 'Во время трансформации вы не можете использовать оружие или заклинания из карт домена, но вы по-прежнему можете использовать другие функции или способности, к которым у вас есть доступ.',
+    examples: 'Примеры:',
+    adv: 'Преимущества при:'
+  }
+}
+
 export const DaggerheartBeastform = (props) => {
   const character = () => props.character;
 
@@ -34,14 +47,19 @@ export const DaggerheartBeastform = (props) => {
     <ErrorWrapper payload={{ character_id: character().id, key: 'DaggerheartBeastform' }}>
       <Show when={character().beastforms.length > 0}>
         <GuideWrapper character={character()}>
-          <div class="blockable p-4">
-            <h2 class="text-lg mb-2 dark:text-snow">{t('daggerheart.beast.transformation')}</h2>
+          <div class="blockable p-4 dark:text-snow">
+            <h2 class="text-lg mb-2">{t('daggerheart.beast.transformation')}</h2>
             <Select
               containerClassList="w-full"
               items={beastformsSelect()}
               selectedValue={character().beastform === null ? 'none' : character().beastform}
               onSelect={(value) => changeBeastform(value)}
             />
+            <Show when={character().beastform}>
+              <p class="mt-2">{translation[locale()]['desc']}</p>
+              <p class="mt-1">{translation[locale()]['examples']} {config.beastforms[character().beastform].examples[locale()]}</p>
+              <p class="mt-1">{translation[locale()]['adv']} {config.beastforms[character().beastform].adv[locale()]}</p>
+            </Show>
           </div>
         </GuideWrapper>
       </Show>
