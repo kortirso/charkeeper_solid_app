b917d8b69d2cdb70776d8b8300cb4224373bcee6
Bogdanov Anton <kortirso@gmail.com>
2025-11-07 13:57:42 +0300

    IS-113 added rendering Daggerheart weapon distances in squares

--- components/substances/Combat.jsx
+++ components/substances/Combat.jsx
@@ -1,35 +1,77 @@
-import { createSignal, For, Show, batch } from 'solid-js';
+import { createSignal, createEffect, For, Show, batch, Switch, Match } from 'solid-js';
 
-import { ErrorWrapper, Dice, GuideWrapper, createModal } from '../../components';
+import { ErrorWrapper, Dice, GuideWrapper, createModal, Button, Select } from '../../components';
 import { useAppState, useAppLocale } from '../../context';
-import { modifier } from '../../helpers';
+import { Edit } from '../../assets';
+import { modifier, readFromCache, writeToCache } from '../../helpers';
 import { fetchTagInfoRequest } from '../../requests/fetchTagInfoRequest';
 
+const DH_SETTINGS_CACHE_NAME = 'DaggerheartSettings';
 const TRANSLATION = {
   en: {
     attack: 'Attack',
     damage: 'Damage',
     distance: 'Range',
     primary: 'Ready to use',
-    additional: 'Reserve'
+    additional: 'Reserve',
+    showSquares: 'Show square distance',
+    settings: 'Settings',
+    distances: {
+      'melee': 'Melee',
+      'very close': 'V Close',
+      'close': 'Close',
+      'far': 'Far',
+      'very far': 'V Far'
+    }
   },
   ru: {
     attack: 'Атака',
     damage: 'Урон',
     distance: 'Дист',
     primary: 'Подготовленное',
-    additional: 'Запасное'
+    additional: 'Запасное',
+    showSquares: 'Дистанция в квадратах',
+    settings: 'Настройки',
+    distances: {
+      'melee': 'Вплотную',
+      'very close': 'Близко',
+      'close': 'Средне',
+      'far': 'Далеко',
+      'very far': 'Оч далеко'
+    }
   }
+}
+const DH_SQUARE_DISTANCES = {
+  'melee': 5,
+  'very close': 15,
+  'close': 30,
+  'far': 60,
+  'very far': '>60'
 }
 
 export const Combat = (props) => {
   const character = () => props.character;
 
+  const [showSettings, setShowSettings] = createSignal(false);
+  const [settings, setSettings] = createSignal([]);
   const [tagInfo, setTagInfo] = createSignal([]);
+  const [lastActiveCharacterId, setLastActiveCharacterId] = createSignal(undefined);
 
   const { Modal, openModal } = createModal();
   const [appState] = useAppState();
   const [locale] = useAppLocale();
+
+  const readDhSettings = async () => {
+    const cacheValue = await readFromCache(DH_SETTINGS_CACHE_NAME);
+    setSettings(cacheValue === null ? [] : cacheValue.split(','));
+  }
+
+  createEffect(() => {
+    if (lastActiveCharacterId() === character().id) return;
+
+    setLastActiveCharacterId(character().id);
+    if (character().provider === 'daggerheart') readDhSettings();
+  });
 
   const showTagInfo = async (tag, value) => {
     const provider = character().provider === 'dnd5' || character().provider === 'dnd2024' ? 'dnd' : character().provider;
@@ -40,11 +82,19 @@ export const Combat = (props) => {
     });
   }
 
+  const updateSettings = (value) => {
+    const newValue = settings().includes(value) ? settings().filter((item) => item !== value) : settings().concat([value]);
+    batch(() => {
+      if (character().provider === 'daggerheart') writeToCache(DH_SETTINGS_CACHE_NAME, newValue.join(','));
+      setSettings(newValue);
+    })
+  }
+
   const renderAttacksBox = (title, values) => {
     if (values.length === 0) return <></>;
 
     return (
-      <div class="p-4 blockable mb-2">
+      <div class="p-4 mb-2">
         <h2 class="text-lg font-normal! mb-2 dark:text-snow">{title}</h2>
         <table class="w-full table first-column-full-width table-top">
           <thead>
@@ -91,7 +141,14 @@ export const Combat = (props) => {
                     <p>{attack.damage}{attack.damage_bonus > 0 ? modifier(attack.damage_bonus) : ''}</p>
                   </td>
                   <td class="p-1 text-center">
-                    <p>{attack.distance || attack.range}</p>
+                    <Show when={character().provider === 'daggerheart'} fallback={<p>{attack.distance || attack.range}</p>}>
+                      <Show
+                        when={settings().includes('showSquares')}
+                        fallback={<p class="text-sm">{TRANSLATION[locale()]['distances'][attack.range]}</p>}
+                      >
+                        <p>{DH_SQUARE_DISTANCES[attack.range]}</p>
+                      </Show>
+                    </Show>
                   </td>
                 </tr>
               }
@@ -105,8 +162,37 @@ export const Combat = (props) => {
   return (
     <ErrorWrapper payload={{ character_id: character().id, key: 'Combat' }}>
       <GuideWrapper character={character()}>
-        {renderAttacksBox(TRANSLATION[locale()]['primary'], character().attacks.filter((item) => item.ready_to_use))}
-        {renderAttacksBox(TRANSLATION[locale()]['additional'], character().attacks.filter((item) => !item.ready_to_use))}
+        <div class="blockable relative">
+          <Show when={showSettings()}>
+            <div class="p-4 pb-0">
+              <Switch>
+                <Match when={character().provider === 'daggerheart'}>
+                  <Select
+                    multi
+                    containerClassList="w-full md:w-1/2 mb-2"
+                    labelText={TRANSLATION[locale()]['settings']}
+                    items={{
+                      'showSquares': TRANSLATION[locale()]['showSquares']
+                    }}
+                    selectedValues={settings() || []}
+                    onSelect={(value) => updateSettings(value)}
+                  />
+                </Match>
+              </Switch>
+            </div>
+          </Show>
+          {renderAttacksBox(TRANSLATION[locale()]['primary'], character().attacks.filter((item) => item.ready_to_use))}
+          {renderAttacksBox(TRANSLATION[locale()]['additional'], character().attacks.filter((item) => !item.ready_to_use))}
+          <Show when={character().provider === 'daggerheart'}>
+            <Button
+              default
+              classList='absolute top-0 right-0 rounded min-w-6 min-h-6 opacity-50 m-0!'
+              onClick={() => setShowSettings(!showSettings())}
+            >
+              <Edit />
+            </Button>
+          </Show>
+        </div>
         <Modal classList="md:max-w-md!">
           <p class="mb-3 text-xl">{tagInfo()[0]}</p>
           <p>{tagInfo()[1]}</p>
