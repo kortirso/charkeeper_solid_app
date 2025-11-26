999ea97a05e4aa92de087ee2a7d3374b6c54c3bd
Bogdanov Anton <kortirso@gmail.com>
2025-11-26 22:48:07 +0300

    render homebrew books

--- requests/fetchHomebrewModulesRequest.jsx
+++
@@ -1,8 +0,0 @@
-import { apiRequest, options } from '../helpers';
-
-export const fetchHomebrewModulesRequest = async (accessToken, provider) => {
-  return await apiRequest({
-    url: `/frontend/homebrews/${provider}/books.json`,
-    options: options('GET', accessToken)
-  });
-}
--- requests/updateUserBookRequest.jsx
+++
@@ -1,8 +0,0 @@
-import { apiRequest, options } from '../helpers';
-
-export const updateUserBookRequest = async (accessToken, id) => {
-  return await apiRequest({
-    url: `/frontend/users/books/${id}.json`,
-    options: options('PATCH', accessToken)
-  });
-}
