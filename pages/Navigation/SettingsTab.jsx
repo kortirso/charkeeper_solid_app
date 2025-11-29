fbabe27c18c002f662ae993884c0d28a021f8af9
Bogdanov Anton <kortirso@gmail.com>
2025-11-29 17:25:44 +0300

    release 0.3.20

--- pages/Navigation/SettingsTab.jsx
+++ pages/Navigation/SettingsTab.jsx
@@ -73,7 +73,7 @@ export const SettingsTab = () => {
             {t('pages.settingsPage.logout')}
           </p>
         </div>
-        <p class="py-3 px-4 dark:text-snow">{t('pages.settingsPage.version')} 0.3.19, 2025.11.28</p>
+        <p class="py-3 px-4 dark:text-snow">{t('pages.settingsPage.version')} 0.3.20, 2025.11.29</p>
       </div>
     </>
   );
