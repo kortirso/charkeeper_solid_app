c83c86af5100fc3345decb189a7ac55b306e7cce
Bogdanov Anton <kortirso@gmail.com>
2025-12-23 18:26:33 +0300

    IS-627 added different distance meters

--- components/substances/Combat.jsx
+++ components/substances/Combat.jsx
@@ -1,4 +1,4 @@
-import { createSignal, createEffect, For, Show, batch, Switch, Match } from 'solid-js';
+import { createSignal, createEffect, createMemo, For, Show, batch } from 'solid-js';
 
 import { ErrorWrapper, Dice, GuideWrapper, createModal, Button, Select } from '../../components';
 import daggerheartConfig from '../../data/daggerheart.json';
@@ -7,7 +7,7 @@ import { Edit } from '../../assets';
 import { modifier, readFromCache, writeToCache } from '../../helpers';
 import { fetchTagInfoRequest } from '../../requests/fetchTagInfoRequest';
 
-const DH_SETTINGS_CACHE_NAME = 'DaggerheartSettings';
+const DISTANCE_SETTINGS_CACHE_NAME = 'DistanceSettings';
 const TRANSLATION = {
   en: {
     attack: 'Attack',
@@ -16,14 +16,20 @@ const TRANSLATION = {
     primary: 'Ready to use',
     additional: 'Reserve',
     showSquares: 'Show square distance',
-    settings: 'Settings',
-    distances: {
+    narrative: 'Narrative distance',
+    imperial: 'Imperial distance',
+    metric: 'Metric distance',
+    settings: 'Distance settings',
+    daggerheart: {
       'melee': 'Melee',
       'very close': 'V Close',
       'close': 'Close',
       'far': 'Far',
       'very far': 'V Far'
-    }
+    },
+    squares: 'sq',
+    feet: 'ft',
+    meters: 'm'
   },
   ru: {
     attack: 'Атака',
@@ -32,29 +38,36 @@ const TRANSLATION = {
     primary: 'Подготовленное',
     additional: 'Запасное',
     showSquares: 'Дистанция в квадратах',
-    settings: 'Настройки',
-    distances: {
+    narrative: 'Нарративная дистанция',
+    imperial: 'Имперская система',
+    metric: 'Метрическая система',
+    settings: 'Настройки дистанции',
+    daggerheart: {
       'melee': 'Вплотную',
       'very close': 'Близко',
       'close': 'Средне',
       'far': 'Далеко',
       'very far': 'Оч далеко'
-    }
+    },
+    squares: 'кв',
+    feet: 'фт',
+    meters: 'м'
   }
 }
 const DH_SQUARE_DISTANCES = {
-  'melee': 5,
-  'very close': 15,
-  'close': 30,
-  'far': 60,
-  'very far': '>60'
+  'melee': 1,
+  'very close': 3,
+  'close': 6,
+  'far': 12,
+  'very far': 24
 }
 
 export const Combat = (props) => {
   const character = () => props.character;
 
   const [showSettings, setShowSettings] = createSignal(false);
-  const [settings, setSettings] = createSignal([]);
+  const [settings, setSettings] = createSignal({});
+
   const [tagInfo, setTagInfo] = createSignal([]);
   const [lastActiveCharacterId, setLastActiveCharacterId] = createSignal(undefined);
 
@@ -62,16 +75,29 @@ export const Combat = (props) => {
   const [appState] = useAppState();
   const [locale] = useAppLocale();
 
-  const readDhSettings = async () => {
-    const cacheValue = await readFromCache(DH_SETTINGS_CACHE_NAME);
-    setSettings(cacheValue === null ? [] : cacheValue.split(','));
+  const readDistanceSettings = async () => {
+    const cacheValue = await readFromCache(DISTANCE_SETTINGS_CACHE_NAME);
+    setSettings(cacheValue === null ? {} : JSON.parse(cacheValue));
   }
 
   createEffect(() => {
     if (lastActiveCharacterId() === character().id) return;
 
     setLastActiveCharacterId(character().id);
-    if (character().provider === 'daggerheart') readDhSettings();
+    readDistanceSettings();
+  });
+
+  const distanceOptions = createMemo(() => {
+    const result = {}
+
+    if (character().provider === 'daggerheart') result.narrative = TRANSLATION[locale()].narrative;
+
+    return {
+      ...result,
+      'showSquares': TRANSLATION[locale()].showSquares,
+      'imperial': TRANSLATION[locale()].imperial,
+      'metric': TRANSLATION[locale()].metric
+    };
   });
 
   const showTagInfo = async (tag, value) => {
@@ -84,9 +110,9 @@ export const Combat = (props) => {
   }
 
   const updateSettings = (value) => {
-    const newValue = settings().includes(value) ? settings().filter((item) => item !== value) : settings().concat([value]);
+    const newValue = { ...settings(), [character().provider]: value }
     batch(() => {
-      if (character().provider === 'daggerheart') writeToCache(DH_SETTINGS_CACHE_NAME, newValue.join(','));
+      writeToCache(DISTANCE_SETTINGS_CACHE_NAME, JSON.stringify(newValue));
       setSettings(newValue);
     })
   }
@@ -105,6 +131,23 @@ export const Combat = (props) => {
     if (dices.length > 0) props.openSimpleDiceRoll(dices, attack.damage_bonus);
   }
 
+  const renderAttackDistance = (attack) => {
+    const provider = character().provider;
+    if (provider === 'daggerheart') {
+      if (settings()[provider] === 'narrative' || settings()[provider] === undefined) {
+        return TRANSLATION[locale()].daggerheart[attack.range];
+      }
+    }
+
+    const squares = provider === 'daggerheart' ? DH_SQUARE_DISTANCES[attack.range] : (attack.distance || attack.range);
+    if (!squares) return '';
+
+    if (settings()[provider] === 'imperial') return `${squares * 5} ${TRANSLATION[locale()].feet}`;
+    if (settings()[provider] === 'metric') return `${squares * 1.5} ${TRANSLATION[locale()].meters}`;
+
+    return `${squares} ${TRANSLATION[locale()].squares}`;
+  }
+
   const renderAttacksBox = (title, values) => {
     if (values.length === 0) return <></>;
 
@@ -115,9 +158,9 @@ export const Combat = (props) => {
           <thead>
             <tr>
               <td />
-              <td class="text-center">{TRANSLATION[locale()]['attack']}</td>
-              <td class="text-center px-1">{TRANSLATION[locale()]['damage']}</td>
-              <td class="text-center px-1">{TRANSLATION[locale()]['distance']}</td>
+              <td class="text-center">{TRANSLATION[locale()].attack}</td>
+              <td class="text-center px-1">{TRANSLATION[locale()].damage}</td>
+              <td class="text-center px-1">{TRANSLATION[locale()].distance}</td>
             </tr>
           </thead>
           <tbody>
@@ -159,14 +202,7 @@ export const Combat = (props) => {
                     </td>
                     <td class="pt-2 pb-1">
                       <div class="flex items-center justify-center h-7">
-                        <Show when={character().provider === 'daggerheart'} fallback={<p>{attack.distance || attack.range}</p>}>
-                          <Show
-                            when={settings().includes('showSquares')}
-                            fallback={<p class="text-sm">{TRANSLATION[locale()]['distances'][attack.range]}</p>}
-                          >
-                            <p>{DH_SQUARE_DISTANCES[attack.range]}</p>
-                          </Show>
-                        </Show>
+                        <p class="text-sm">{renderAttackDistance(attack)}</p>
                       </div>
                     </td>
                   </tr>
@@ -214,33 +250,24 @@ export const Combat = (props) => {
         <div class="blockable relative">
           <Show when={showSettings()}>
             <div class="p-4 pb-0">
-              <Switch>
-                <Match when={character().provider === 'daggerheart'}>
-                  <Select
-                    multi
-                    containerClassList="w-full md:w-1/2 mb-2"
-                    labelText={TRANSLATION[locale()]['settings']}
-                    items={{
-                      'showSquares': TRANSLATION[locale()]['showSquares']
-                    }}
-                    selectedValues={settings() || []}
-                    onSelect={(value) => updateSettings(value)}
-                  />
-                </Match>
-              </Switch>
+              <Select
+                containerClassList="w-full md:w-1/2 mb-2"
+                labelText={TRANSLATION[locale()].settings}
+                items={distanceOptions()}
+                selectedValue={settings()[character().provider]}
+                onSelect={updateSettings}
+              />
             </div>
           </Show>
           {renderAttacksBox(TRANSLATION[locale()]['primary'], character().attacks.filter((item) => item.ready_to_use))}
           {renderAttacksBox(TRANSLATION[locale()]['additional'], character().attacks.filter((item) => !item.ready_to_use))}
-          <Show when={character().provider === 'daggerheart'}>
-            <Button
-              default
-              classList='absolute top-0 right-0 rounded min-w-6 min-h-6 opacity-50 m-0!'
-              onClick={() => setShowSettings(!showSettings())}
-            >
-              <Edit />
-            </Button>
-          </Show>
+          <Button
+            default
+            classList='absolute top-0 right-0 rounded min-w-6 min-h-6 opacity-50 m-0!'
+            onClick={() => setShowSettings(!showSettings())}
+          >
+            <Edit />
+          </Button>
         </div>
         <Modal classList="md:max-w-md!">
           <p class="mb-3 text-xl">{tagInfo()[0]}</p>
