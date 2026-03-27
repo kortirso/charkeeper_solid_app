a99e1da7ecd606567ac4eea2c04b4eea3fa6c3cf
Bogdanov Anton <kortirso@gmail.com>
2026-03-27 11:11:28 +0300

    release 0.4.19

--- pages/Navigation/SettingsTab.jsx
+++ pages/Navigation/SettingsTab.jsx
@@ -105,7 +105,7 @@ export const SettingsTab = () => {
             </p>
           </Show>
         </div>
-        <p class="py-3 px-4 dark:text-snow">{t('pages.settingsPage.version')} 0.4.18, 2026.03.25</p>
+        <p class="py-3 px-4 dark:text-snow">{t('pages.settingsPage.version')} 0.4.19, 2026.03.27</p>
       </div>
     </>
   );
